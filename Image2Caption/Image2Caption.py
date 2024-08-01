f45ea6786ec1bd8f54992f43d21adf3c28964160
Manoj Kumar <manoj.kuma@media.net>
2024-08-01 18:49:33 +0530

    adds vector db for data storage

---
+++ Image2Caption/Image2Caption.py
@@ -0,0 +1,72 @@
+# Libraries
+import base64
+import io
+from openai import OpenAI
+from Credentials import *
+
+
+from PIL import Image
+import requests
+import os
+
+def getImage(source):
+    try:
+        # Check if the source is a URL
+        if source.startswith("http://") or source.startswith("https://"):
+            response = requests.get(source, stream=True)
+            response.raise_for_status()
+            image = Image.open(response.raw)
+        else:
+            # Assume the source is a file path
+            if not os.path.exists(source):
+                raise FileNotFoundError(f"The file path {source} does not exist.")
+            image = Image.open(source)
+            
+        return image
+    except (requests.exceptions.RequestException, FileNotFoundError, IOError) as e:
+        print(f"Error in get_image...take action \n Error: {e}")
+
+
+def encode_image(image):
+  buffered = io.BytesIO()
+  image.save(buffered, format="PNG")
+  image_bytes = buffered.getvalue()
+  
+  # Encode the image bytes to base64
+  return base64.b64encode(image_bytes).decode('utf-8')
+
+client = OpenAI(api_key = OPEN_AI_API_KEY)
+
+def generateImageCaption(base64_image):
+    try:
+        response = client.chat.completions.create(
+            model=IMAGE2TEXTMODEL,
+            messages=[
+                {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text", 
+                        "text": "Describe the image in atmost 50 tokens. Make sure to complete the description. Give me whole context of image with different elements into it"
+                    },
+                    {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                    },
+                ],
+                }
+            ],
+            max_tokens=50,
+        )
+        return response.choices[0].message.content
+    except:
+        return "Error in API Calling...Take Action before too it's get too late"
+    
+
+def getImageCaption(url):
+    image = getImage(url)
+    base64_image = encode_image(image)
+    caption = generateImageCaption(base64_image)
+    return caption
---
+++ recursion_read.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import random
+
+module_path = './VectorDB'
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    
+module_path2 = './Image2Caption'
+if module_path2 not in sys.path:
+    sys.path.append(module_path2)
+
+import Image2Caption
+import chroma
+import Credentials
+
+
+
+vector = chroma.vectordb(r"/Users/manoj.kuma/Project/Media-Hackathon/Team-MVPs/VectorDB/ChromaDB", "image-collection", Credentials.OPEN_AI_API_KEY)
+
+i = 0
+
+def register_images(path: str):
+    global i
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        if os.path.isdir(full_path):
+            register_images(full_path)
+        else:
+            i = i+1
+            if(i<2):  
+                caption = Image2Caption.getImageCaption(full_path)
+                vector.add_embeddings(str(random.getrandbits(128)), caption, full_path)
+                
+register_images(r"/Users/manoj.kuma/Project/Media-Hackathon/Team-MVPs/DataSet")
+print(i)
